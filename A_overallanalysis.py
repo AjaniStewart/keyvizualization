162c614ffa13a6cfc851b877f38ace03a3d0d58c
AjaniStewart <theamjaha@gmail.com>
2016-08-10 11:32:02 -0400

    Update A_overallanalysis.py

--- A_overallanalysis.py
+++ A_overallanalysis.py
@@ -1,32 +1,43 @@
-
-# coding: utf-8
-
-# In[3]:
-
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use("ggplot")
 
 df = pd.read_csv("C:\\Users\\NYUK12STEM\\Desktop\\Batch_2485769_batch_results.csv")
-only_main = df[["Input.img_id", "Answer.ans"]]
+r = df[(df["WorkTimeInSeconds"] > 15)] #to attempt to remove results from people who didn't even try(25% of people) fuck 'em
+only_main = r[["Input.img_id", "Answer.ans"]]
 s = pd.DataFrame({'Count':only_main.groupby(["Input.img_id", "Answer.ans"]).size()}).reset_index()
 s.set_index(s["Input.img_id"])
 s["Percent"] = pd.Series(s["Count"]/20 * 100, index=s.index)
+s = s[(s["Answer.ans"] == "Yes")]
+#s[(s["Input.img_id"].str.contains("_bn"))]["Count"].sum()
 
-location = pd.DataFrame(columns=["Location", "Number"])
-color = pd.DataFrame(columns=["Colors","Number"])
-rowc = [["BN",47], ["LBN",40], ["LBB", 38], ["WN",42], ["WLB",49],["WB",39]]
+location = pd.DataFrame(columns=["Location", "Number Wrong"])
+color = pd.DataFrame(columns=["Colors","Number Wrong"])
+rowc = [["BN",s[(s["Input.img_id"].str.contains("_bn"))]["Count"].sum()],
+        ["LBN",s[(s["Input.img_id"].str.contains("_lbn"))]["Count"].sum()],
+        ["LBB", s[(s["Input.img_id"].str.contains("_lbb"))]["Count"].sum()],
+        ["WN",s[(s["Input.img_id"].str.contains("_wn"))]["Count"].sum()],
+        ["WLB",s[(s["Input.img_id"].str.contains("_wlb"))]["Count"].sum()],
+        ["WB",s[(s["Input.img_id"].str.contains("_wb"))]["Count"].sum()]]
 for row in rowc:
     color.loc[len(color)] = row
-color["Percentage"] = pd.Series(color.Number / 80 * 100)
-color.set_index("Color", inplace=True)
+color["Percentage"] = pd.Series(color["Number Wrong"] / 80 * 100)
+color.set_index("Colors", inplace=True)
+#color.to_csv("c:\\Users\\NYUK12STEM\\Desktop\\color_results.csv")
 print color
 color.plot(kind="bar")
-rows = [["Bot_left",63],["Top_Left",67],["Bot_Right",61],["Top_Right",64]]
+rows = [["Bot_left",s[(s["Input.img_id"].str.contains("bot_left"))]["Count"].sum()],
+        ["Top_Left",s[(s["Input.img_id"].str.contains("top_left"))]["Count"].sum()],
+        ["Bot_Right",s[(s["Input.img_id"].str.contains("bot_right"))]["Count"].sum()],
+        ["Top_Right",s[(s["Input.img_id"].str.contains("top_right"))]["Count"].sum()]]
 for row in rows:
     location.loc[len(location)] = row
-location["Percentage"] = pd.Series(location.Number / 120 * 100)
+location["Percentage"] = pd.Series(location["Number Wrong"] / 120 * 100)
+#location.to_csv("c:\\Users\\NYUK12STEM\\Desktop\\location_results.csv")
 print location
-location.set_index("Location", inplace=True) #Fixed!!!
+location.set_index("Location", inplace=True)
 location.plot(kind="bar")
 plt.show()
 
